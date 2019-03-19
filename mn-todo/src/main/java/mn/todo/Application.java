7b8ed98e18fa7d4dd0b198de70e2e37711a5512d
Sigmund Hansen <sigmund.hansen@schibsted.no>
2019-03-19 10:24:54 +0100

    Adds solution to Assignment 1: Hello World.

--- mn-todo/src/main/java/mn/todo/Application.java
+++ mn-todo/src/main/java/mn/todo/Application.java
@@ -1,8 +1,22 @@
 package mn.todo;
 
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
+import java.util.Optional;
+
+@Controller("/hello")
 public class Application {
+
+    @Get(uri="{/name}",produces = MediaType.TEXT_PLAIN)
+    public String hello(@Nullable String name) {
+        return String.format("Hello, %s!", Optional.ofNullable(name).orElse("World"));
+    }
 
     public static void main(String[] args) {
         Micronaut.run(Application.class);
